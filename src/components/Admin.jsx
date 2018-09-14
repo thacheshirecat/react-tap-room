b4da059dfb547659a40b21e458650f85798f59ce
Cheshire Cat <thacheshirecat@gmail.com>
2018-09-14 11:46:50 -0700

    Add a link at the top of the admin page that allows users to navigate to /admin/newbrew which currently does not exist.

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import BrewList from './BrewList';
 
@@ -6,7 +7,13 @@ function Admin(props)
 {
   return (
     <div>
+      <style jsx>{`
+            h3 {
+              text-align: center;
+            }
+      `}</style>
       <h2>Admin</h2>
+      <h3><Link to='/admin/newbrew'>Add New Brew</Link></h3>
       <BrewList />
     </div>
   );
