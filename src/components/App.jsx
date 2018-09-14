01d3955218b7c5c580a1f3f531b9b3ed63499327
Cheshire Cat <thacheshirecat@gmail.com>
2018-09-14 12:08:09 -0700

    Successfully add state to app, refractor BrewList and App to handle the new state object being passed down from App.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { v4 } from 'uuid';
 
 import Header from './Header';
 import Home from './Home';
 import BrewList from './BrewList';
 import About from './About';
 import Admin from './Admin';
+import NewBrew from './NewBrew';
 import Error404 from './Error404';
 
 class App extends React.Component
@@ -15,42 +17,64 @@ class App extends React.Component
   {
     super(props);
     this.state = {
-      masterBrewList = {},
+      masterBrewList: {
+        'a67gsd-a97yjd-hoj-huehuehue' : {
+          name: 'Ruby Zozzle',
+          brewer: 'Hi-Wheel',
+          description: 'Sparkling Wine & Grapefruit',
+          abv: '6.8%',
+          price: '7',
+          remaining: '20'
+        },
+        '984fgon-z5b190-7dfmz-jajaja' : {
+          name: 'Tart N Juicy',
+          brewer: 'Epic',
+          description: 'Sour IPA',
+          abv: '4.5%',
+          price: '6',
+          remaining: '60'
+        }
+      },
       selectedBrew: null
     };
   }
 
 
-
-  return (
-    <div className="container">
-      <style jsx global>{`
-          h2 {
-            margin-top: 10px;
-            color: green;
-            text-align: center;
-          }
-          .card {
-            background-color: gold;
-            padding: 10px;
-          }
-          body {
-            background-image: url(https://raw.githubusercontent.com/thacheshirecat/react-tap-room/master/src/assets/images/bg.jpg);
-            background-repeat: no-repeat;
-            background-size: cover;
-          }
-      `}</style>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/brews' component={BrewList} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='/admin' component={Admin} />
-        <Route exact path='/admin/newbrew' component={NewBrew} />
-        <Route component={Error404} />
-      </Switch>
-    </div>
-  );
+  render()
+  {
+    return (
+      <div className="container">
+        <style jsx global>{`
+            h2 {
+              margin-top: 10px;
+              color: green;
+              text-align: center;
+            }
+            .card {
+              background-color: gold;
+              padding: 10px;
+            }
+            body {
+              background-image: url(https://raw.githubusercontent.com/thacheshirecat/react-tap-room/master/src/assets/images/bg.jpg);
+              background-repeat: no-repeat;
+              background-size: cover;
+            }
+        `}</style>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route
+            exact path='/brews'
+            render={()=>
+              <BrewList brewList={this.state.masterBrewList} />} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/admin' component={Admin} />
+          <Route exact path='/admin/newbrew' component={NewBrew} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/BrewList.jsx
+++ src/components/BrewList.jsx
@@ -1,74 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Brew from './Brew';
 
-const masterBrewList = [
-  {
-    name: 'Ruby Zozzle',
-    brewer: 'Hi-Wheel',
-    description: 'Sparkling Wine & Grapefruit',
-    abv: '6.8%',
-    price: '7',
-    remaining: '20'
-  },
-  {
-    name: 'Tart N Juicy',
-    brewer: 'Epic',
-    description: 'Sour IPA',
-    abv: '4.5%',
-    price: '6',
-    remaining: '60'
-  },
-  {
-    name: 'Hamm\'s',
-    brewer: 'Miller/Coors',
-    description: 'American Lager',
-    abv: '4.7%',
-    price: '3',
-    remaining: '65'
-  },
-  {
-    name: 'Prismatic',
-    brewer: 'Ninkasi',
-    description: 'Juicy IPA',
-    abv:  '5.9%',
-    price: '6',
-    remaining: '75'
-  },
-  {
-    name: 'Juicy Haze',
-    brewer: 'New Belgium',
-    description: 'India Pale Ale',
-    abv:  '7.5%',
-    price: '6',
-    remaining: '18'
-  },
-  {
-    name: '8 Hop',
-    brewer: 'New Belgium',
-    description: 'Pale Ale',
-    abv:  '5.5%',
-    price: '6',
-    remaining: '58'
-  }
-];
-
-function BrewList(){
+function BrewList(props){
   return(
     <div>
       <h2>Brews</h2>
       <hr />
-      {masterBrewList.map((brew, index) =>
-        <Brew
+      {Object.keys(props.brewList).map(function(brewId) {
+        var brew = props.brewList[brewId];
+        return <Brew
           name={brew.name}
           brewer={brew.brewer}
           description={brew.description}
           abv={brew.abv}
           price={brew.price}
           remaining={brew.remaining}
-          key={index}/>
-      )}
+          key={brewId}
+          brewId={brewId}/>
+      })}      
     </div>
   );
 }
+
+BrewList.propTypes = {
+  brewList: PropTypes.object
+};
 
 export default BrewList;
